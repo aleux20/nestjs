f17eb22cf2f4013edabdbdd496de02b7c11e537f
Hans Afanador <hansalejandroca@gmail.com>
2025-03-16 13:45:01 -0500

    Services

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller({})
 export class TaskController {
+  constructor(private tasksService: TasksService) {}
+
   @Get('/tasks')
   getAllTasks() {
-    return 'All tasks';
+    return this.tasksService.getTasks();
   }
 }
