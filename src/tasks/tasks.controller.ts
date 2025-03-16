a457b1beec6e1a89318b5e88d9cbfe5b20edca23
Hans Afanador <hansalejandroca@gmail.com>
2025-03-16 13:57:16 -0500

    Metodos HTTP

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
-@Controller({})
+@Controller('/tasks')
 export class TaskController {
   constructor(private tasksService: TasksService) {}
 
-  @Get('/tasks')
+  @Get()
   getAllTasks() {
     return this.tasksService.getTasks();
+  }
+
+  @Post()
+  createTask() {
+    return this.tasksService.createTask();
+  }
+
+  @Put()
+  updateTask() {
+    return this.tasksService.updateTask();
+  }
+
+  @Delete()
+  deleteTask() {
+    return this.tasksService.deleteTask();
+  }
+
+  @Patch()
+  patchTask() {
+    return this.tasksService.patchTask();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,4 +5,20 @@ export class TasksService {
   getTasks() {
     return ['Task 1', 'Task 2', 'Task 3'];
   }
+
+  createTask() {
+    return 'Create task';
+  }
+
+  updateTask() {
+    return 'Update task';
+  }
+
+  deleteTask() {
+    return 'Delete task';
+  }
+
+  patchTask() {
+    return 'Patch task';
+  }
 }
