9db43a0a48ee6eae6981701f6975dc91e1942016
Hans Afanador <hansalejandroca@gmail.com>
2025-03-17 12:04:09 -0500

    Response

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,23 +1,41 @@
-import { Controller, Delete, Get, Patch, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('/tasks')
 export class TaskController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks() {
+  getAllTasks(@Query() query: any) {
+    console.log(query);
     return this.tasksService.getTasks();
   }
 
+  @Get('/:id')
+  getTask(@Param('id') id: string) {
+    return this.tasksService.getTask(parseInt(id));
+  }
+
   @Post()
-  createTask() {
-    return this.tasksService.createTask();
+  createTask(@Body() task: CreateTaskDto) {
+    return this.tasksService.createTask(task);
   }
 
   @Put()
-  updateTask() {
-    return this.tasksService.updateTask();
+  updateTask(@Body() task: UpdateTaskDto) {
+    return this.tasksService.updateTask(task);
   }
 
   @Delete()
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,16 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+export interface Task extends CreateTaskDto {
+  id: number;
+}
+
+export interface User {
+  name: string;
+  age: number;
+}
 
 @Injectable()
 export class TasksService {
+  private tasks: Task[] = [];
+
   getTasks() {
-    return ['Task 1', 'Task 2', 'Task 3'];
+    return this.tasks;
   }
 
-  createTask() {
-    return 'Create task';
+  getTask(id: number) {
+    const taskFound = this.tasks.find((task) => task.id === id);
+
+    if (!taskFound) {
+      return new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    return taskFound;
   }
 
-  updateTask() {
+  createTask(task: CreateTaskDto) {
+    this.tasks.push({
+      ...task,
+      id: this.tasks.length + 1,
+    });
+    return task;
+  }
+
+  updateTask(task: UpdateTaskDto) {
+    console.log(task);
     return 'Update task';
   }
 
