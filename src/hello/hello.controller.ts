5221ef2042b3868702e63b5aceab94ad39c377cd
Hans Afanador <hansalejandroca@gmail.com>
2025-03-18 15:58:05 -0500

    Http status error

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpCode, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 
 @Controller()
@@ -9,5 +9,23 @@ export class HelloController {
     response.status(200).json({
       message: 'Hello World!',
     });
+  }
+
+  @Get('something')
+  @HttpCode(201)
+  somethingNew() {
+    return 'This is something new';
+  }
+
+  @Get('not-found')
+  @HttpCode(404)
+  notFoundPage() {
+    return 'This page is not found';
+  }
+
+  @Get('error')
+  @HttpCode(500)
+  errorPage() {
+    return 'This page is error';
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
-  private users = [
+  private users: any[] = [
     { id: 1, name: 'John Doe', phone: '1234567890' },
     { id: 2, name: 'Jane Doe', phone: '0987654321' },
   ];
 
   getUsers() {
     return this.users;
+  }
+
+  createUser(user: CreateUserDto) {
+    this.users.push(user);
+    return {
+      ...user,
+      id: this.users.length + 1,
+    };
   }
 }
